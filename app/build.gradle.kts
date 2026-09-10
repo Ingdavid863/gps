080a8d6abbbecd371e536b3643777a24c624dca9
Ingdavid863 <albajimenezdavid8@gmail.com>
2026-09-10 10:32:49 -0600

    Fix MapLibre dependency for APK build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("org.maplibre.gl:android-sdk-vulkan-opengl:13.4.1")
+    implementation("org.maplibre.gl:android-sdk:13.3.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
