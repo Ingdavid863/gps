f00f33a2705742b8429dd0bf1c5a7fe7fde86937
Ingdavid863 <albajimenezdavid8@gmail.com>
2026-09-10 10:00:54 -0600

    Agregar configuración Gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories { google(); mavenCentral() }
+}
+rootProject.name = "GPS3D_AR_David"
+include(":app")
